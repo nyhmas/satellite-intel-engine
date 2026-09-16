881f6efc64b9cca3b195e92e78d59bcb2508e5f2
nyhmas <nyhmas@gmail.com>
2026-09-16 22:07:07 +0200

    feat: add rest-api-feat-18 integration

--- src/index.ts
+++ src/index.ts
@@ -41,3 +41,4 @@ export * from './rest-api-feat-14.js';
 export * from './rest-api-feat-15.js';
 export * from './rest-api-feat-16.js';
 export * from './rest-api-feat-17.js';
+export * from './rest-api-feat-18.js';
